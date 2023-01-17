6de8e61a818b76e1c496801471f91e4a56cd0819
Chris Beaumont <chris.beaumont@gmail.com>
2023-01-16 22:07:52 -0800

    added comments and TODO to previous script

--- tag_script.py
+++ tag_script.py
@@ -1,4 +1,9 @@
 # Script to affect (add, remove, change) tags in Obsidian files
+# ToDo
+# 2022-01-16
+# - Recursive into directories instead of ignoring them
+# - Function that takes the content, the string to change, the change to make, add/remove/change
+
 import os
 
 input_dir = '/Users/chrisbeaumont/chris.beaumont@gmail.com - Google Drive/My Drive/Obsidian/BULLET JOURNAL 2023/04 - PEOPLE/STATESPACE/ENGINEERING'
@@ -9,11 +14,17 @@ for file in files:
 
     if file_path.endswith('.md'):
         print(file_path)
+
+        # Get the file's contents
         f = open(file_path, 'r')
         content = f.read()
+        f.close()
+        
+        # Do what you want to do to the tags
         content = content.replace("#Collection", '')
         print(content)
-        f.close()
+        
+        # Overwrite the original file with the ammended content
         f = open(file_path, 'w')
         f.write(content)
         f.close()
